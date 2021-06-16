e8caabb2abfb4fbf929b2c8b5628ef7cd79750aa
SimarjeetSinghRajput <simarjeet.rajput+bitbucket@teamlance.io>
2021-06-16 12:46:47 +0530

    updated code

--- src/container/dm.js
+++ src/container/dm.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, { Component } from 'react';
 import {
   StyleSheet,
@@ -17,44 +9,67 @@ import {
   SafeAreaView,
   StatusBar,
 } from 'react-native';
-import { openUrl } from '../helper/openUrl'
+import { openUrl } from '../helper/openUrl';
+import CountryPicker from 'react-native-country-picker-modal';
 
 
 class DirectMessage extends Component {
   constructor(props) {
     super(props);
+
+
     this.state = {
-      phoneNumber: ''
+      phoneNumber: '',
+      callingCode: '91',
+      cca2: 'IN',
     }
+  }
+
+  onSelect = (country) => {
+    this.setState({
+      callingCode: country.callingCode,
+      cca2: country.cca2,
+    });
   }
 
 
   render() {
-    const { phoneNumber } = this.state;
+    const { phoneNumber, cca2, callingCode } = this.state;
     return (
 
 
-      <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
+      <SafeAreaView style={styles.safearea}>
         <StatusBar backgroundColor='black' barStyle='light-content' />
         <View style={styles.container}>
-          <TextInput
-            value={phoneNumber}
-            onChangeText={(text) => this.setState({ phoneNumber: text })}
-            placeholderTextColor={'gray'}
-            placeholder={'e g +919864836495'}
-            style={styles.textInput}
-            keyboardType="phone-pad"
-          />
+          <View style={styles.suncontainer}>
+            <CountryPicker
+              withFilter
+              countryCode={cca2}
+              withFlag
+              withCallingCode
+              onSelect={(country) => this.onSelect(country)}
+            />
+            <Text style={styles.textStyle}>{`+${callingCode}`}</Text>
+            <TextInput
+              value={phoneNumber}
+              onChangeText={(text) => this.setState({ phoneNumber: text })}
+              placeholderTextColor='green'
+              placeholder={'Enter your number'}
+              style={styles.textInput}
+              keyboardType="phone-pad"
+              underlineColorAndroid='green'
+            />
+          </View>
           <Text style={styles.text}>{'Direct message via text and whatsapp'}</Text>
           <View style={styles.buttonContainer}>
             <TouchableOpacity onPress={() => openUrl(
-              `sms:${phoneNumber
+              `sms:${callingCode+phoneNumber
               }`
             )}>
               <Image source={require('../assets/images/message.jpeg')} resizeMode='cover' style={styles.messageImage} />
             </TouchableOpacity>
             <TouchableOpacity onPress={() => openUrl(
-              `whatsapp://send?phone=${phoneNumber
+              `whatsapp://send?phone=${callingCode+phoneNumber
               }`
             )}>
               <Image source={require('../assets/images/whatsapp.png')} resizeMode='cover' style={styles.image} />
@@ -68,20 +83,28 @@ class DirectMessage extends Component {
 
 
 const styles = StyleSheet.create({
+
+  safearea:{
+    flex: 1, 
+    backgroundColor: 'black'
+  },
   container: {
     flex: 1,
-    paddingHorizontal: 10,
+    paddingHorizontal: 5,
     marginTop: 20,
     backgroundColor: 'black',
   },
-
+  suncontainer:{
+    flexDirection: 'row', 
+    alignItems: 'center'
+  },
   textInput: {
-    height: 50,
     borderBottomColor: 'white',
-    borderWidth: 2,
-    marginTop: 30,
-    color: '#fff',
-    fontSize: 22
+    color: 'red',
+    left: 10,
+    width: '100%',
+    padding: 10,
+    fontSize: 18,
   },
   text: {
     color: '#fff',
@@ -103,6 +126,11 @@ const styles = StyleSheet.create({
   image: {
     height: 80,
     width: 80
+  },
+  textStyle: {
+    color: '#fff',
+    fontSize: 18,
+    textAlign: 'center',
   }
 
 });
